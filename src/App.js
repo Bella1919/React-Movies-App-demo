5845bf808dbd6b4316072a15317c49459e0b3f49
Bella D <bella14378899@gmail.com>
2021-03-04 16:14:28 +1300

    add router and navBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import {Route,Redirect,Switch} from 'react-router-dom';
 import Movies from './components/movies';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
+import NavBar from './components/navBar';
 import './App.css';
+
+
 
 function App() {
   return (
-  <main className='container'>
-    <Movies />
-  </main>
+    <React.Fragment>
+      <NavBar />
+      <main className='container'>
+        <Switch>
+          <Route path="/movies" component={Movies}/>
+          <Route path="/customers" component={Customers}/>
+          <Route path="/rentals" component={Rentals}/>
+          <Route path="/not-found" component={NotFound}/>
+          {/* Set when URL meet "/" will auto jump to page Movies */}
+          {/* Put the exact to make sure it will set Movies as home page */}
+          <Redirect from="/"  exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
