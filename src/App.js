8c4101522b9027f085bf3fca81f05803aa4472b8
Bella D <bella14378899@gmail.com>
2021-03-04 17:00:37 +1300

    add movies link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Customers from './components/customers';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
 import NavBar from './components/navBar';
+import MovieForm from './components/movieForm';
 import './App.css';
 
 
@@ -15,6 +16,7 @@ function App() {
       <NavBar />
       <main className='container'>
         <Switch>
+          <Route path="/movies/:id" component={MovieForm}/>
           <Route path="/movies" component={Movies}/>
           <Route path="/customers" component={Customers}/>
           <Route path="/rentals" component={Rentals}/>
--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
-const MovieForm = () => {
+const MovieForm = ({match,history}) => {
     return ( 
-        <h1>
-            MovieForm
-        </h1>
+        <div>
+            <h1>
+                {/* Dynamic to show the movie's id we need to use router's match.params.id property */}
+                MovieForm {match.params.id}
+            </h1>
+            <button 
+                className="btn btn-primary"
+                // history.push is a property of router, it can dump to anyone URL which you want to go.
+                onClick={()=>history.push('/movies')}
+            >
+                Save
+            </button>
+        </div>
      );
 }
  
