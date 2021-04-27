46a9f0a083ad8a3efc231eefca2ba9bfc9daf400
Bella D <bella14378899@gmail.com>
2021-04-27 18:45:20 +1200

    finish extract config

--- src/services/genreService.js
+++ src/services/genreService.js
@@ -1,8 +1,8 @@
 import http from './httpService'
-import config from '../config.json'
+import {apiUrl} from '../config.json'
 
 
 
 export function getGenres() {
-    return http.get(config.apiUrl+'/genres')
+    return http.get(apiUrl+'/genres')
   }
